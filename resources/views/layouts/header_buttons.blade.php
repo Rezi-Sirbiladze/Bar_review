85a92509387851f3929d2362959f6ddd01d36250
Rezi <rezi.sirbiladze2@gmail.com>
2022-06-13 18:33:37 +0200

    minor fixes

--- resources/views/layouts/header_buttons.blade.php
+++ resources/views/layouts/header_buttons.blade.php
@@ -4,13 +4,10 @@
         <nav class="catalog_filter">
             <ul class="filter_wrapper">
                 <li class="filter_item">
-                <button class="filter_btn" type="button" id="1"  onClick="buscar(this.id)">Todos</button>
+                <button class="filter_btn" type="button" id="1"  onClick="buscar(this.id)">Nombre</button>
                 </li>
                 <li class="filter_item">
                 <button class="filter_btn" type="button" id="2"  onClick="buscar(this.id)">Valoracion</button>
-                </li>
-                <li class="filter_item">
-                <button class="filter_btn" type="button" id="3"  onClick="buscar(this.id)">Comments</button>
                 </li>
                 <li class="filter_item">
                 <button class="filter_btn" type="button" id="4"  onClick="buscar(this.id)">Fecha creacion</button>
